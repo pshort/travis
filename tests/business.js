3ea54a84b5e3f0ae82a8991deb2a576aac186394
Peter Short <peter.short@gmail.com>
2015-11-18 11:13:32 +0000

    tests

--- tests/business.js
+++ tests/business.js
@@ -1,9 +1,18 @@
 var should = require('should');
+var express = require('express');
+var request = require('supertest');
+
+var app = require('../index');
 
 describe("The api", function() {
 	it("should return 200 hello world", function(done) {
 		// do the testa
-		(4).should.be.exactly(5);
-		done();
+		request(app)
+		  .get('/')
+		  .expect(200)
+		  .end(function(err, res) {
+	            if(err) throw err;
+		    done();
+		  });
 	});
 });
